aa77333a939f768f817e7c41181fb82428b466ee
JeronimoGarp <jeronimo.garcia26502@ucaldas.edu.co>
2023-10-02 13:41:31 -0500

    Realizacion de controladores de las entidades (user y menu)

--- backend/ms-seguridad/src/controllers/index.ts
+++ backend/ms-seguridad/src/controllers/index.ts
@@ -5,3 +5,5 @@ export * from './rol-menu.controller';
 export * from './menu-rol.controller';
 export * from './rol-user.controller';
 export * from './user-rol.controller';
+export * from './user.controller';
+export * from './menu.controller';
