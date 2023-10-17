97249b4847c33df714c375eb303c42cecd09f275
JeronimoGarp <jeronimo.garcia26502@ucaldas.edu.co>
2023-10-17 16:54:36 -0500

    Se realizo la creacion de las variables de entorno para proteger el servidor por medio de un .env y se instalo la libreria dotenv

--- backend/ms-seguridad/src/config/seguridad.config.ts
+++ backend/ms-seguridad/src/config/seguridad.config.ts
@@ -1,11 +1,12 @@
 export namespace configuracionSeguridaad{
     //esta sera nuestra clave
-    export const claveJWT:string = "Admin@2023*";
+    export const claveJWT = process.env.SECRET_PASSWORD_JWT;
     export const menuUsuarioId = "651b1081deb9531aa4a636ef";
     export const listarAccion = "listar";
     export const editarAccion = "editar";
     export const eliminarAccion = "eliminar";
     export const guardarAccion = "guardar";
     export const descargarAccion = "descargar";
+    export const mongoDbConnectionString  = process.env.CONNECTION_STRING_MONGODB;
     
 }
