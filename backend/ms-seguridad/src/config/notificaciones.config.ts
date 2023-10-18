d2c925c0dd0bc97df9acdffad8c733010a46426e
Juan Guzmán <guzmanhenaojuanesteban@gmail.com>
2023-10-18 00:31:01 -0500

    Recuperación de contraseña por notificación SMS

--- backend/ms-seguridad/src/config/notificaciones.config.ts
+++ backend/ms-seguridad/src/config/notificaciones.config.ts
@@ -1,3 +1,4 @@
 export namespace ConfiguracionNotificaciones {
   export const urlNotificaciones2fa: string = "http://127.0.0.1:5000/email"
+  export const urlNotificacionesSms: string = "http://127.0.0.1:5000/sms"
 }
